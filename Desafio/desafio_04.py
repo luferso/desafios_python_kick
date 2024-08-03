3ab26664afe00cba2105128f0c1b0e31be392bf4
Luan Soares <81059066+luferso@users.noreply.github.com>
2024-08-03 19:21:38 -0300

    Delete Desafio directory

--- Desafio/desafio_04.py
+++
@@ -1,9 +0,0 @@
-print(' = '*11 + 'CONVERSOR DE TEMPERATURA' + ' = '*11)
-temp_C = float(input('Insira a temperatura em °C: '))
-
-temp_K = (temp_C + 273)
-temp_F = (temp_C *1.8)+32
-print(' ')
-print(' ')
-print(f'A temperatura {temp_C}°C é equivalente a {temp_K}K na escola Kelvin ou {temp_F}°F na escala Fahrenheit')
-print(' = '*30)
