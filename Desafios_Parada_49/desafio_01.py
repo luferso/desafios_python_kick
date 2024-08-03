a4c3f266dbb3027c716efbd0262a4ca2b26a11cc
Luan Soares <81059066+luferso@users.noreply.github.com>
2024-08-03 19:22:13 -0300

    Add files via upload

---
+++ Desafios_Parada_49/desafio_01.py
@@ -0,0 +1,9 @@
+# Faça um programa em que o usuário digita dois valores e o resultado da soma é exibido na tela.
+
+print('='*10 + ' CALCULADORA DE ADIÇÃO ' + '='*10)
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+soma = num1 + num2
+print('='*16 + ' RESULTADO ' + '='*16)
+print(f'{num1} + {num2} = {soma}')
+print('='*43)
---
+++ Desafios_Parada_49/desafio_05.py
@@ -0,0 +1,13 @@
+#Faça um programa que calcule a área de um quadrado/retângulo.
+
+print('='*10 + ' CALCULADORA DE ÁREA ' + '='*10)
+print('')
+altura = float(input('Insira a medida da altura em metros: '))
+largura = float(input('Insira a medida da largura em metros: '))
+print('')
+area = altura * largura
+print('='*15 + ' RESULTADO ' + '='*15)
+print('')
+print(f'A área calculada é de {area}m.')
+print('')
+print('='*41)
