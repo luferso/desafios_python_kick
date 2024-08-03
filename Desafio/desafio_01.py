6bd5907474e59906d5aba4ca9610501f8a4cef29
Luan Soares <81059066+luferso@users.noreply.github.com>
2024-08-03 19:14:46 -0300

    Add files via upload

---
+++ Desafio/desafio_01.py
@@ -0,0 +1,7 @@
+print('='*10 + ' CALCULADORA DE ADIÇÃO ' + '='*10)
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+soma = num1 + num2
+print('='*16 + ' RESULTADO ' + '='*16)
+print(f'{num1} + {num2} = {soma}')
+print('='*43)
